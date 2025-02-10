42c9202eda90ad432f47c81cb9bbfbbd6082a215
mertsekerunity <mertseker9393@gmail.com>
2025-02-10 13:05:01 +0100

    client transform scripts etc are added.
    client transform scripts etc are added.

---
+++ Assets/Scripts/ClientNetworkTransform.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode.Components;
+using UnityEngine;
+
+public class ClientNetworkTransform : NetworkTransform
+{
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        CanCommitToTransform = IsOwner;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        CanCommitToTransform = IsOwner;
+
+        base.Update();
+
+        if (NetworkManager != null)
+        {
+            if (NetworkManager.IsConnectedClient || NetworkManager.IsListening)
+            {
+                if (CanCommitToTransform)
+                {
+                    TryCommitTransformToServer(transform, NetworkManager.LocalTime.Time);
+                }
+            }
+        }
+    }
+
+    protected override bool OnIsServerAuthoritative()
+    {
+        return false;
+    }
+}

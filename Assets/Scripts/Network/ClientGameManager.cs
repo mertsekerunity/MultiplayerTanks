282282447b95cd65afdd8958c4411c4b9ab33685
mertsekerunity <mertseker9393@gmail.com>
2025-02-12 12:03:06 +0100

    unity relay and other related stuff added.
    unity relay and other related stuff added.

--- Assets/Scripts/Network/ClientGameManager.cs
+++ Assets/Scripts/Network/ClientGameManager.cs
@@ -1,12 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
 using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ClientGameManager
 {
+    JoinAllocation allocation;
+
     const string MenuSceneName = "Main Menu";
 
     public async Task<bool> InitAsync()
@@ -26,5 +34,26 @@ public class ClientGameManager
     public void GoToMenu()
     {
         SceneManager.LoadScene(MenuSceneName);
+    }
+
+    public async Task StartClientAsync(string joinCode)
+    {
+        try
+        {
+            allocation = await Relay.Instance.JoinAllocationAsync(joinCode);
+        }
+
+        catch(Exception ex)
+        {
+            Debug.Log(ex);
+            return;
+        }
+
+        UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+
+        RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+        transport.SetRelayServerData(relayServerData);
+
+        NetworkManager.Singleton.StartClient();
     }
 }
--- Assets/Scripts/Network/HostGameManager.cs
+++ Assets/Scripts/Network/HostGameManager.cs
@@ -1,8 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HostGameManager
 {
-    
+    Allocation allocation;
+    string joinCode;
+
+    const int MaxConnections = 20;
+    const string GameSceneName = "Game";
+
+    public async Task StartHostAsync()
+    {
+        try
+        {
+            allocation = await Relay.Instance.CreateAllocationAsync(MaxConnections);
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex);
+            return;
+        }
+
+        try
+        {
+            joinCode = await Relay.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            Debug.Log(joinCode);
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex);
+            return;
+        }
+
+        UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+
+        RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+        transport.SetRelayServerData(relayServerData);
+
+        NetworkManager.Singleton.StartHost();
+
+        NetworkManager.Singleton.SceneManager.LoadScene(GameSceneName, LoadSceneMode.Single);
+    }
 }
